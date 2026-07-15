c234f3312ae0a19fb567d2a788aecabe81a26a01
ujjawalpaawani04 <ujjawal.paawanigroup@gmail.com>
2026-07-15 15:18:10 +0530

    updated home page

--- src/pages/Contact/ContactPage.jsx
+++ src/pages/Contact/ContactPage.jsx
@@ -1,17 +1,19 @@
 import { ContactHero } from "./components/ContactHero";
 import { ContactForm } from "./components/ContactForm";
 import { WhyContactUs } from "./components/WhyContactUs";
-import { ProcessSection } from "./components/ProcessSection";
-import { ContactFAQ } from "./components/ContactFAQ";
+import LocationMap from "./components/LocationMap";
+// import { ProcessSection } from "./components/ProcessSection";
+// import { ContactFAQ } from "./components/ContactFAQ";
 
 const ContactPage = () => {
   return (
     <div className="bg-white">
       <ContactHero />
       <ContactForm />
-      <WhyContactUs />
-      <ProcessSection />
-      <ContactFAQ />
+      {/* <WhyContactUs /> */}
+      <LocationMap />
+      {/* <ProcessSection /> */}
+      {/* <ContactFAQ /> */}
     </div>
   );
 };
---
+++ src/pages/Home/components/WhyChooseUs.jsx
@@ -0,0 +1,7 @@
+const WhyChooseUs = () => {
+  return (
+    <div>WhyChooseUs</div>
+  )
+}
+
+export default WhyChooseUs
